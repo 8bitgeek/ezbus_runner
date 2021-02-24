da810815b08a75682b875489f156e7178140ee37
8bitgeek <mike@8bitgeek.net>
2021-02-23 23:16:13 -0500

    clean compile syslog & setup linker errors

--- src/ezbus_setup.c
+++ src/ezbus_setup.c
@@ -1,4 +1,3 @@
-
 /*****************************************************************************
 * Copyright © 2021 Mike Sharkey <mike@8bitgeek.net>                          *
 *                                                                            *
@@ -20,30 +19,50 @@
 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        *
 * DEALINGS IN THE SOFTWARE.                                                  *
 *****************************************************************************/
-#include <ezbus_runner.h>
-#include <ezbus.h>
-#include <ezbus_flip.h>
-#include <ezbus_port.h>
 #include <ezbus_setup.h>
-#include <syslog.h>
-#include <syslog_printf.h>
+#include <ezbus_port.h>
+#include <ezbus_mac_pause.h>
 
-extern int ezbus_runner(ezbus_udp_cmdline_t* ezbus_udp_cmdline)
+extern void ezbus_callback_setup( ezbus_udp_cmdline_t* ezbus_udp_cmdline, ezbus_port_t* port )
 {
-    return -1;
+    ezbus_port_init_struct( port );
+
+    ezbus_platform_port_set_name(port,NULL);
+    ezbus_platform_port_set_handle(port,0);
 }
 
-
-extern bool ezbus_socket_callback_send ( ezbus_socket_t socket )
+extern bool ezbus_platform_set_tx( ezbus_platform_port_t* port, bool enable )
 {
-    return false;
+    // if ( enable )
+    // {
+    //     caribou_gpio_set( ezbus_platform_port_get_dir_gpio_tx(port) );
+    // }
+    // else
+    // {
+    //     ezbus_platform_drain(port);
+    //     caribou_gpio_reset( ezbus_platform_port_get_dir_gpio_tx(port) );
+    // }
+    return enable;
 }
 
-extern bool ezbus_socket_callback_recv ( ezbus_socket_t socket )
+extern bool ezbus_pause_callback( ezbus_mac_t* mac )
 {
+    switch( ezbus_mac_pause_get_state( mac ) )
+    {
+        case ezbus_pause_state_start:
+            break;
+
+        case ezbus_pause_state_half_duration_timeout:  
+            break;
+        
+        case ezbus_pause_state_duration_timeout:
+            break;
+        
+        case ezbus_pause_state_finish:
+            break;
+
+        default:
+            break;
+    }
     return true;
-}
-
-extern void ezbus_socket_callback_closing ( ezbus_socket_t socket )
-{
 }
